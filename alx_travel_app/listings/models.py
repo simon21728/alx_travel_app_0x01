11d3d942d0b8fc6ae0973d9e962317cb280de54b
Sewmehon Bayu <simonbayu2211@gmail.com>
2025-10-02 04:30:40 -0300

    API Development for Listings and Bookings in Django

---
+++ alx_travel_app/listings/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Booking(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bookings')
+    booked_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.listing.title} booked by {self.booked_by.username}"
